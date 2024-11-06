86689104e94d3354e794b2e56d833a966deb1258
Pierre Gleize <gleizepierre3gmail.com>
2024-11-06 17:18:35 +0100

    Pierre route delete fonctionelle

--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,40 @@ const Trip = require("../model/trips");
 const Cart = require("../model/carts");
 const moment = require("moment");
 
-router.post("/carts", (req, res) => {
-  const {
-    /*id à recuperer du front*/
-  } = req.body;
+router.post("/", (req, res) => {
+  const { id } = req.body;
 
   const newCart = new Cart({
     paiement: true,
-    /*id à recuperer du front: id à recuperer du front,*/
+    info: id,
   });
-
+  console.log(newCart);
   newCart.save().then(() => {
     res.json({
-      /*id à recuperer du front: id à recuperer du front,*/ paiement: true,
+      paiement: true,
     });
   });
 });
 
-router.get("/carts", (req, res) => {
+router.get("/", (req, res) => {
   Cart.find()
     .populate("info")
     .then((carts) => {
       res.json(carts);
     });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const deleteCart = await Cart.deleteOne({ info: id });
+  console.log(deleteCart);
+  if (deleteCart.deletedCount > 0) {
+    const cart = await Cart.find();
+    res.json({ result: "Success", cart: cart });
+  } else {
+    res.json({ result: "Fail" });
+  }
 });
 
 module.exports = router;
